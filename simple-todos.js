7174feac6e3d222469d137f89da39d39b2d7315d
Weiming Zhai <wmzhai@gmail.com>
2015-03-17 00:42:06 -0700

    11.Publish and subscribe

--- simple-todos.js
+++ simple-todos.js
@@ -2,6 +2,8 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
 
+  Meteor.subscribe("tasks");
+
   Template.body.helpers({
    tasks: function () {
     if (Session.get("hideCompleted")) {
@@ -39,10 +41,13 @@ if (Meteor.isClient) {
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
     }
+  });
 
-  }
-
-  )
+  Template.task.helpers({
+      isOwner: function () {
+        return this.owner === Meteor.userId();
+      }
+    });
 
 
   // In the client code, below everything else
@@ -55,6 +60,10 @@ if (Meteor.isClient) {
     "click .delete": function () {
       
       Meteor.call("deleteTask", this._id);
+    },
+    // Add an event for the new button to Template.task.events
+    "click .toggle-private": function () {
+      Meteor.call("setPrivate", this._id, ! this.private);
     }
   });
  
@@ -82,10 +91,27 @@ Meteor.methods({
     });
   },
   deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+
     Tasks.remove(taskId);
   },
   setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+
     Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+    // Add a method to Meteor.methods called setPrivate
+  setPrivate: function (taskId, setToPrivate) {
+ 
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   }
 });
 
@@ -93,7 +119,13 @@ Meteor.methods({
 
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  
+  Meteor.publish("tasks",function(){
+    return Tasks.find({
+    $or: [
+      { private: {$ne: true} },
+      { owner: this.userId }
+    ]
+    });
+  })
 }
