8f63e68af0388b342f11060ea870b8b9cb42977b
Weiming Zhai <wmzhai@gmail.com>
2015-03-16 20:17:34 -0700

    finish 3.Collections

--- simple-todos.js
+++ simple-todos.js
@@ -1,20 +1,15 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
 
   Template.body.helpers({
-    tasks: [
-      { text : "This is task 1"},
-      { text : "This is task 2"},
-      { text : "This is task 3"}
-    ]
-
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    tasks: function(){
+      return Tasks.find({});
     }
+
   });
+
+ 
 }
 
 if (Meteor.isServer) {
