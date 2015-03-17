e01d659483e35c70e91a0f52979dd0e5ccb07ba5
Weiming Zhai <wmzhai@gmail.com>
2015-03-16 20:35:06 -0700

    4.Forms and events

--- simple-todos.js
+++ simple-todos.js
@@ -4,11 +4,32 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({});
+      //按插入顺序逆序
+      return Tasks.find({}, {sort: {createAt : -1}});
     }
 
-  });
+ });
 
+  Template.body.events({
+    "submit .new-task": function (event){
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createAt: new Date()
+      });
+
+      //添加以后清空这一行的内容
+      event.target.text.value = "";
+
+      //告诉浏览器不需要使用默认的submit处理（会刷新），因为我们已经处理过了这个消息
+      //如果取消这一行，浏览器会有一个刷新过程
+      return false;
+    }
+
+  }
+
+  )
  
 }
 
