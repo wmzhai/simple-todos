dcb253f8e598e02fb3ea9b52e6eee7724f28f1fb
Weiming Zhai <wmzhai@gmail.com>
2015-03-16 23:02:01 -0700

    10.Security with methods

--- simple-todos.js
+++ simple-todos.js
@@ -25,12 +25,7 @@ if (Meteor.isClient) {
     "submit .new-task": function (event){
       var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(),            // current time
-        owner: Meteor.userId(),           // _id of logged in user
-        username: Meteor.user().username  // username of logged in user
-      });
+      Meteor.call("addTask", text)
 
       //添加以后清空这一行的内容
       event.target.text.value = "";
@@ -53,11 +48,13 @@ if (Meteor.isClient) {
   // In the client code, below everything else
   Template.task.events({
     "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      
+      Meteor.call("setChecked", this._id,  !this.checked);
+
     },
     "click .delete": function () {
-      Tasks.remove(this._id);
+      
+      Meteor.call("deleteTask", this._id);
     }
   });
  
@@ -67,6 +64,33 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+
+// At the bottom of simple-todos.js, outside of the client-only block
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
+
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
